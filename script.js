e74762a0557f6357094a51c04ae877ab999d5294
Gabriel Ferla <ferlagamer@hotmail.com>
2023-04-27 19:17:18 -0300

    Iniciar quiz

--- script.js
+++ script.js
@@ -39,34 +39,6 @@ function validatePhone(){
     }
   
 }
-function calc(){
-    let num1 = Number(prompt("Digite o primeiro número"))
-    let num2 = Number(prompt("Digite o segundo número"))
-    let op =Number(prompt(`Escolha uma opção: ${num1} e ${num2} \n[1]-Somar  \n[2]-Subtrair \n[3]-Multiplicar \n[4]-Dividir`))
-
-    let msg =document.getElementById('msg')
-
-    msg.innerHTML += `<h2>Resultado</h2>`
-
-    switch(op){
-        case 1 :
-            msg.innerHTML += `<p> ${num1}  +${num2} = <strong>${num1 + num2}</strong>`
-            break
-        case 2 :
-            msg.innerHTML += `<p> ${num1}  -${num2} = <strong>${num1 - num2}</strong>`
-            break;
-        case 3 :
-            msg.innerHTML += `<p> ${num1}  *${num2} = <strong>${num1 * num2}</strong>`
-                break;
-        case 4 :
-             msg.innerHTML += `<p> ${num1}  /${num2} = <strong>${num1 / num2}</strong>`
-                break;
-                default:
-                    msg.innerHTML += `<p>Opção Inválida</p>`
-                break;
-        
-    }
-}
 
 function validar(){
     if(validateEmail() == true && validateNome() == true && validatePhone() == true){
@@ -74,5 +46,15 @@ function validar(){
     }else{
         alert("Preencha os campos corretamente")
     }
+}
+
+function changeBackgroundColor(value) {
+    
+    const body = document.querySelector('body')
+    body.style.backgroundColor = value
+}
+
+function iniciarQuiz(){
+    var nome = prompt("Digite seu nome: ")
 }
 
